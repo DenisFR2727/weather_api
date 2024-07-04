3611ec4c0d16469fa5963cfd4315687f67262d86
Denis <dh92fr@gmail.com>
2024-07-04 19:11:17 +0300

    thermometr

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "./App.css";
 import Weather from "./components/weather/Weather";
 
 function App() {
+  document.body.addEventListener("touchmove", function (e) {
+    e.preventDefault();
+  });
   return (
     <div className="App">
       <Weather />
